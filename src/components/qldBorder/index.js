1abb05a3a5eeeb4d1c385b2f4e2bcc411638ec45
Idam Adam <contact@idamadam.com>
2020-10-10 12:44:56 +1100

    Remove embed & gif. Only send a sentence.

--- src/components/qldBorder/index.js
+++ src/components/qldBorder/index.js
@@ -1,9 +1,8 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
-const randomGif = require('../../shared/randomGif');
-const acceptedMessages = require('./acceptedMessages');
-const constructMessageEmbed = require('./constructEmbed');
+const randomGif = require("../../shared/randomGif");
+const acceptedMessages = require("./acceptedMessages");
 
 async function checkIfOpen(msg) {
   const response = await fetch("https://covidlive.com.au/");
@@ -11,32 +10,58 @@ async function checkIfOpen(msg) {
   const $ = cheerio.load(responseBody);
 
   // Number of days VIC needs to have completed without a mystery case.
-  const qldThreshold = 28
+  const qldThreshold = 28;
 
-  const unknownCases = $("h3:contains('Days Since Last Community Transmission')").next().children();
-  const vicUnknownDaysString = unknownCases.find("a:contains('VIC')").parent().find('p').text().trim();
+  const unknownCases = $(
+    "h3:contains('Days Since Last Community Transmission')"
+  )
+    .next()
+    .children();
+  const vicUnknownDaysString = unknownCases
+    .find("a:contains('VIC')")
+    .parent()
+    .find("p")
+    .text()
+    .trim();
 
-  const lastUpdated = $('td.LAST_UPDATED').text();
   const vicUnknownDays = parseInt(vicUnknownDaysString);
 
   if (isNaN(vicUnknownDays)) {
-      console.error("ERROR: Can't find case number");
-      msg.channel.send("Oh no, i cawn't find the numbews tuwu figuwe out if queenswand iws open tuwu victowians.");
-      return;
+    console.error("ERROR: Can't find case number");
+    msg.channel.send(
+      "Oh no, i cawn't find the numbews tuwu figuwe out if queenswand iws open tuwu victowians."
+    );
+    return;
   }
 
-  if (vicUnknownDays < qldThreshold) {
-    msg.channel.send(await randomGif('no'));
-    msg.channel.send(constructMessageEmbed({ vicUnknownDays, qldThreshold, lastUpdated }));
-    return;
-  } else {
-    msg.channel.send(await randomGif('yes'));
-    msg.channel.send(constructMessageEmbed({ thresholdPassed: true, vicUnknownDays, qldThreshold, lastUpdated }));
-    return;
+  const daysToGo = qldThreshold - vicUnknownDays;
+  
+  let progressUpdate = `It's been ${vicUnknownDays} days since Victoria's last mystery case.`;
+
+  if (daysToGo > 0) {
+    progressUpdate = progressUpdate.concat(` ${daysToGo} days to go.`);
+  }
+
+  switch (true) {
+    case daysToGo > 21:
+      msg.channel.send(`**Not yet.** ${progressUpdate}`);
+      break;
+    case (daysToGo <= 21 && daysToGo > 14):
+      msg.channel.send(`**We're making progress.** ${progressUpdate}`);
+      break;
+    case (daysToGo <= 14 && daysToGo > 7):
+        msg.channel.send(`**We've passed the halfway point.** ${progressUpdate}`);
+        break;
+    case (daysToGo <= 7 && daysToGo > 0):
+        msg.channel.send(`**SO CLOSE.** ${progressUpdate}`);
+        break;
+    case (daysToGo <= 0):
+        msg.channel.send(`**YES!** ${progressUpdate}`);
+        msg.channel.send(await randomGif('yes'));
   }
 }
 
 module.exports = {
   acceptedMessages,
-  checkIfOpen
-}
+  checkIfOpen,
+};
