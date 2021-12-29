4356af9ae2833fe15ebdefa029a5ee72aed06293
Idam Adam <contact@idamadam.com>
2021-12-30 00:24:37 +1000

    Initial commit for add score

---
+++ src/components/wordle/addScore.js
@@ -0,0 +1,63 @@
+const Database = require('sqlite-async');
+const crypto = require('crypto');
+const { getScoreSql } = require('./getScores');
+
+const wordleRegex = /Wordle \d* \d\/\d/;
+
+async function processScoreSubmit(message, client) {
+  const scoreMessage = message.content
+  console.log(message);
+  const wordleStringArray = scoreMessage.match(/Wordle \d* \d\/\d/)[0].split(' ')
+  const day = wordleStringArray[1]
+  const score = wordleStringArray[2].charAt(0);
+  message.react('👀');
+  try {
+    await addScore({
+      user_id: message.author.id,
+      day: day,
+      score: score
+    });
+    message.react('✅');
+  } catch(error) {
+    message.react('🚫');
+    if (error.message == 'SQLITE_CONSTRAINT: UNIQUE constraint failed: games.id') {
+      const channel = client.channels.cache.get(message.channelId)
+      channel.send({
+        content: `**You've already submitted a score for puzzle #${day}.** \nScores can only be submitted once per puzzle.`,
+        reply: {
+          messageReference: message.id
+        }
+      });
+    } else {
+        console.error(error)
+        const channel = client.channels.cache.get(message.channelId)
+        channel.send({
+          content: `\:rotating_light: **Something went wrong with the bot, this score wasn't saved.** \:rotating_light: \n Debug: \`${error.message}\` \n cc <@689432634740441120>`,
+          reply: {
+            messageReference: message.id
+          }
+        });
+    }
+  }
+}
+
+async function addScore({ user_id, day, score }) {
+  const entryId = crypto.createHash('md5').update(`${user_id} ${day}`).digest('hex');
+
+  const sql = `INSERT INTO 
+                games (id, user_id, timestamp, day, score)
+              VALUES 
+                ('${entryId}', ${user_id}, datetime('now'), ${day}, ${score})`
+
+  const db = await Database.open('./db/rodeo.db');
+  await db.run(sql)
+  const data = await db.all(getScoreSql)
+  console.log(data);
+  await db.close();
+}
+
+
+module.exports = {
+  processScoreSubmit,
+  wordleRegex
+};
