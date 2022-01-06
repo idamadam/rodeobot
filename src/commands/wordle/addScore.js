cd0df9a88ab24a5d9bb6d0a8576f5c1be1fe09e3
Idam Adam <contact@idamadam.com>
2022-01-06 21:53:13 +1000

    Simplify addscore

--- src/commands/wordle/addScore.js
+++ src/commands/wordle/addScore.js
@@ -5,42 +5,32 @@ const { getScoreSql } = require('./getScores');
 const wordleRegex = /Wordle \d* \d\/\d/;
 
 async function processScoreSubmit(message, client) {
-  const scoreMessage = message.content
-  const wordleStringArray = scoreMessage.match(/Wordle \d* \d\/\d/)[0].split(' ')
+  const wordleStringArray = message.content.match(/Wordle \d* \d\/\d/)[0].split(' ')
   const day = wordleStringArray[1]
   const score = wordleStringArray[2].charAt(0);
+  
   message.react('👀');
+  
   try {
-    await addScore({
-      user_id: message.author.id,
-      day: day,
-      score: score
-    });
+    await addScore({ user_id: message.author.id, day: day, score: score });
     message.react('✅');
     console.log(`Saved Wordle score for ${message.author.username}. Game #${day} - Score ${score}/6`);
   } catch(error) {
     message.react('🚫');
+    const channel = client.channels.cache.get(message.channelId);
     if (error.message == 'SQLITE_CONSTRAINT: UNIQUE constraint failed: games.id') {
       console.log(`Detected duplicate submission for ${message.author.username}. Game #${day}`);
-      const channel = client.channels.cache.get(message.channelId)
       channel.send({
         content: `**You've already submitted a score for puzzle #${day}.** \nScores can only be submitted once per puzzle.`,
-        reply: {
-          messageReference: message.id
-        }
+        reply: { messageReference: message.id }
       });
       return;
-    } else {
-      console.error(error)
-      const channel = client.channels.cache.get(message.channelId)
-      channel.send({
-        content: `\:rotating_light: **Something went wrong with the bot, this score wasn't saved.** \:rotating_light: \n Debug: \`${error.message}\` \n cc <@689432634740441120>`,
-        reply: {
-          messageReference: message.id
-        }
-      });
-      return
     }
+    channel.send({
+      content: `\:rotating_light: **Something went wrong with the bot, this score wasn't saved.** \:rotating_light: \n Debug: \`${error.message}\` \n cc <@689432634740441120>`,
+      reply: { messageReference: message.id }
+    });
+    throw error;
   }
 }
 
