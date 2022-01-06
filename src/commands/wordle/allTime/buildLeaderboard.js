9f41ba2e91f7079effb37e8bdfe619ee675ceedb
Idam Adam <contact@idamadam.com>
2022-01-06 23:37:44 +1000

    Abstract embeds & leaderboards into separate functions

--- src/commands/wordle/allTime/buildLeaderboard.js
+++ src/commands/wordle/allTime/buildLeaderboard.js
@@ -1,30 +1,30 @@
 const _ = require('underscore');
-const getRankText = require('./getRankText');
 
 function buildLeaderboard(scores, rankCount = 1) {
   // Determine the maximum rank, this is used in the for loop below
   const maxRank = _.max(scores, 'rank').rank;
   
-  let fields = [];
+  let leaderboard = [];
 
   // Iterate over every rank and construct an embed field for each rank
   for (let rank = 1; rank <= maxRank; rank++) {
-    const scoresWithRank = _.filter(scores, function(score) { return rank == score.rank });
-    const playersWithRank = scoresWithRank.map((score) => { return `<@${score.user_id}>` }).join('\n');
+    const scoresWithRank = _.filter(scores, score => rank == score.rank);
+    const playersWithRank = scoresWithRank.map(score => score.user_id);
     const rankScore = scoresWithRank[0].score;
-    
-    const embedField = {
-      name: getRankText(rankCount),
-      inline: true,
-      value: playersWithRank.concat('\n', `${rankScore} points`)
-    }
 
-    // Add to rank depending on how many people are within a rank.
-    rankCount = rankCount + scoresWithRank.length;
-    fields.push(embedField);
+    playersWithRank.forEach(playerId => {
+      leaderboard.push({
+        rank: rankCount,
+        score: rankScore,
+        user_id: playerId
+      })
+    })
+
+      // Add to rank depending on how many people are within a rank.
+      rankCount = rankCount + scoresWithRank.length;
   }
 
-  return fields;
+  return leaderboard;
 }
 
 module.exports = buildLeaderboard;
